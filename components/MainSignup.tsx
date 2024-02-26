b2c991203efb7084cbc0f3fb4a9de0248925e5de
Libraula Benard <libraula@gmail.com>
2024-02-26 11:48:32 +0000

    kerina

--- components/MainSignup.tsx
+++ components/MainSignup.tsx
@@ -4,23 +4,17 @@ const MainSignup = () => {
     return (
         <div>
             <section>
-                <div>
-                    <Image
-                        src="https://rb.gy/p2hphi"
-                        width={1980}
-                        height={1080}
-                        alt='netbg'
-                        className='brightness-50 h-[90vh] sm:h-[80vh] md:h-[70vh] lg:h-[60vh] xl:h-[50vh]'
-                    />
+                <div className="relative w-full h-[95vh] sm:h-[80vh] md:h-[70vh] lg:h-[60vh] xl:h-[50vh] 2xl:h-[450px] lg:h-screen xl:h-screen 2xl:h-screen">
+                    <div className="absolute inset-0 bg-cover bg-center brightness-50" style={{ backgroundImage: "url('https://rb.gy/p2hphi')" }}>
+                        {/* Optional: Add content inside the container */}
+                    </div>
                 </div>
-
-
 
                 <div className='absolute top-24 sm:top-24 md:top-24 lg:top-24 xl:top-24'>
                     <div className='text-white w-full sm:px-10 md:px-20' >
-                    <h1 className='text-center text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-6xl font-bold'>
-    Unlimited Books, Comics, Manga and more.
-</h1>
+                        <h1 className='text-center text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-6xl font-bold'>
+                            Unlimited Books, Comics, Manga and more.
+                        </h1>
 
 
                         <p className='text-center text-lg sm:text-xl md:text-xl lg:text-xl xl:text-xl'>Your very own personal Library</p>
