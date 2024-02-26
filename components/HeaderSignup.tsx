adbefd57a999c79bc2420535977f8f605504116a
Libraula Benard <libraula@gmail.com>
2024-02-26 10:04:17 +0000

    delis

--- components/HeaderSignup.tsx
+++ components/HeaderSignup.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const HeaderSignup = () => {
   return (
-    <header>
+    <header className='flex items-center z-50 absolute w-full'>
         <div>
         <img
           src="https://rb.gy/ulxxee"
@@ -12,15 +12,15 @@ const HeaderSignup = () => {
         />
         </div>
 
-        <div>
-            <div>
-            <select name="language" id="language">
+        <div className='flex items-center absolute right-3'>
+            <div className='px-2'>
+            <select name="language" id="language" className='bg-transparent outline outline-1 outline-white text-white p-1'>
                 <option value="English">English</option>
                 <option value="Luganda">Luganda</option>
             </select>
             </div>
             <div>
-                <button>
+                <button className='bg-[#f01621] text-base p-1 rounded-sm'>
                     Sign In
                 </button>
             </div>
