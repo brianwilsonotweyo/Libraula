c3351ec3cb14a398be1d4b9c96c1c56cf29e8aca
Libraula Benard <libraula@gmail.com>
2024-02-26 12:56:08 +0000

    styles

--- components/MainSignup.tsx
+++ components/MainSignup.tsx
@@ -10,7 +10,7 @@ const MainSignup = () => {
                     </div>
                 </div>
 
-                <div className='absolute top-24 sm:top-24 md:top-24 lg:top-24 xl:top-24'>
+                <div className='absolute top-24 sm:top-24 md:top-24 lg:top-20 xl:top-24'>
                     <div className='text-white w-full sm:px-10 md:px-20' >
                         <h1 className='text-center text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-6xl font-bold'>
                             Unlimited Books, Comics, Manga and more.
@@ -19,16 +19,73 @@ const MainSignup = () => {
 
                         <p className='text-center text-lg sm:text-xl md:text-xl lg:text-xl xl:text-xl'>Your very own personal Library</p>
                         <p className='text-center text-lg sm:text-xl md:text-xl lg:text-xl xl:text-xl py-2'>Ready to start reading? Enter your phone number to create an account or restart your membership.</p>
-                        <div className='flex flex-col mt-3 px-5'>
-                            <input className='py-2 px-2' type="text" placeholder='Phone Number' />
-                            <div className='text-center py-2'>
-                                <button className='bg-[#f01621] text-base p-1 rounded-sm'>Get Started</button>
-                            </div>
+
+                        <div className='flex flex-col mt-3 px-5 items-center sm:flex-row sm:justify-center sm:space-y-2 sm:space-x-4'>
+                            <input
+                                className='py-2 px-4 sm:py-3 sm:px-6 sm:text-lg rounded-md border border-white bg-transparent mb-2 sm:mb-0'
+                                type="text"
+                                placeholder='Phone Number'
+                                onChange={(e) => console.log(e.target.value)} // Ensure the input field is functional
+                            />
+                            <button className='bg-[#f01621] text-base px-4 py-2 sm:px-6 sm:py-3 rounded-md'>Get Started</button>
                         </div>
+
+
                     </div>
                 </div>
 
             </section>
+
+            <section className='mt-2'>
+                <div className='py-10 flex items-center  justify-center flex-col lg:flex-row xl:px-20 '>
+                    <div className='text-white text-center lg:text-start px-10'>
+                        <h1 className='text-4xl md:text-6xl '>
+                            Read at yout leisure.
+                        </h1>
+
+                        <p className='text-xl md:text-2xl py-4'>
+                            Read as many Novels, Manga, Comics and Magazines as you'd like, wherever and whenever.
+                        </p>
+                    </div>
+
+                    <div className='w-48 lg:w-1/2 h-auto lg:h-auto mb-8 lg:mb-0'>
+                        <Image
+                            src="https://rb.gy/p2hphi"
+                            alt="Description of the image"
+                            width={300} // Adjust the width as needed
+                            height={200} // Adjust the height as needed
+                            className='object-cover'
+                        />
+                    </div>
+                </div>
+            </section>
+
+            <section className='mt-2 bg-black'>
+                <div className='flex flex-col lg:flex-row justify-center items-center lg:px-20 xl:px-32'>
+                    {/* Image */}
+                    <div className='w-48 lg:w-1/2 h-auto lg:h-auto mb-8 lg:mb-0'>
+                        <Image
+                            src="https://rb.gy/p2hphi"
+                            alt="Description of the image"
+                            width={300} // Adjust the width as needed
+                            height={200} // Adjust the height as needed
+                            className='object-cover'
+                        />
+                    </div>
+
+                    <div className='text-white text-center lg:text-start px-10'>
+                        <h1 className='text-4xl md:text-6xl '>
+                            Read at yout leisure.
+                        </h1>
+
+                        <p className='text-xl md:text-2xl py-4'>
+                            Read as many Novels, Manga, Comics and Magazines as you'd like, wherever and whenever.
+                        </p>
+                    </div>
+                </div>
+            </section>
+
+
         </div>
     )
 }
